ae4bd2915ca12f295b800921dde25ebcbf09ad5f
unknown <alissonoreis@gmail.com>
2023-09-11 18:06:15 -0300

    first commit

---
+++ controleContas/Conta.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleContas
+{
+    class Conta
+    {
+        public long Numero { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        private Agencia agencia;
+
+        public Agencia GetAgencia()
+        {
+            return agencia;
+        }
+
+        public void SetAgencia(Agencia value)
+        {
+            agencia = value;
+        }
+
+        public Cliente Titular { get; set; }
+
+
+        private static decimal saldoTotalGeral = 0;
+        private static Conta contaComMaiorSaldo;
+
+        public Conta(long numero, decimal saldoInicial, Cliente titular, Agencia agencia)
+        {
+            if (saldoInicial < 10.0m)
+            {
+                throw new ArgumentException("Saldo inicial deve ser maior que R$10,00. ");
+            }
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldoInicial;
+            SetAgencia(agencia);
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+            saldoTotalGeral += valor;
+            AtualizarContaComMaiorSaldo();
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if ((Saldo - valor - 0.1m) < 0)
+            {
+                return false;
+            }
+
+            Saldo -= (valor + 0.1m);
+            saldoTotalGeral -= (valor + 0.1m);
+            AtualizarContaComMaiorSaldo();
+            return true;
+        }
+
+        public bool Transferir(Conta destino, decimal valor)
+        {
+            if (Sacar(valor))
+            {
+                destino.Depositar(valor);
+                return true;
+            }
+            return false;
+        }
+
+
+        private void AtualizarContaComMaiorSaldo()
+        {
+            if (contaComMaiorSaldo == null || Saldo > contaComMaiorSaldo.Saldo)
+            {
+                contaComMaiorSaldo = this;
+            }
+        }
+
+
+        public static decimal ObterSaldoTotalGeral()
+        {
+            return saldoTotalGeral;
+        }
+
+
+        public static long ObterNumeroContaComMaiorSaldo()
+        {
+            return contaComMaiorSaldo != null ? contaComMaiorSaldo.Numero : -1;
+        }
+
+        public class Banco
+        {
+            public int Numero { get; set; }
+            public string Nome { get; set; }
+        }
+
+        public class Agencia
+        {
+            public int Numero { get; set; }
+            public string CEP { get; set; }
+            public string Telefone { get; set; }
+            public Banco Banco { get; set; }
+        }
+
+    }
+}
---
+++ controleContas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using controleContas;
+using static controleContas.Conta;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            Banco banco1 = new Banco { Numero = 1, Nome = "Banco XYZ" };
+            Agencia agencia1 = new Agencia { Numero = 123, CEP = "12345-678", Telefone = "123-456-7890", Banco = banco1 };
+            Cliente cliente1 = new Cliente("Alfredo", 2003, "12345678910");
+            Cliente cliente2 = new Cliente("Pedro", 2000, "78942587652");
+            Conta conta1 = new Conta(123456, 1000, cliente1, agencia1);
+            Conta conta2 = new Conta(654321, 11, cliente2, agencia1);
+            Console.WriteLine("O saldo inicial da conta {0} é {1}", conta1.Numero, conta1.Saldo);
+            Console.WriteLine("O saldo inicial da conta {0} é {1}", conta2.Numero, conta2.Saldo);
+
+            conta1.Depositar(1000);
+            Console.WriteLine("O saldo atual da conta {0} é {1}", conta1.Numero, conta1.Saldo);
+            Console.WriteLine("O saldo atual da conta {0} é {1}", conta2.Numero, conta2.Saldo);
+
+            conta1.Transferir(conta2, 300m);
+            Console.WriteLine("O saldo atual da conta {0} é {1}", conta1.Numero, conta1.Saldo);
+            Console.WriteLine("O saldo atual da conta {0} é {1}", conta2.Numero, conta2.Saldo);
+            Console.WriteLine("O cliente da conta {0} é {1}", conta1.Numero, conta1.Titular.Nome);
+
+            decimal saldoTotal = conta1.Saldo + conta2.Saldo;
+            Console.WriteLine("O saldo total das duas contas é {0}", saldoTotal);
+
+            long numeroContaComMaiorSaldo = Conta.ObterNumeroContaComMaiorSaldo();
+            Console.WriteLine("O número da conta com o maior saldo é {0}", numeroContaComMaiorSaldo);
+
+            decimal saldoInicialTotalGeral = Conta.ObterSaldoTotalGeral();
+            Console.WriteLine("O saldo inicial total geral é {0}", saldoInicialTotalGeral);
+
+            
+            Console.WriteLine("Informações da Conta:");
+            Console.WriteLine($"Número da Conta: {conta1.Numero}");
+            Console.WriteLine($"Saldo da Conta: {conta1.Saldo:C}");
+            Console.WriteLine($"Titular da Conta: {conta1.Titular.Nome}");
+
+            
+            Console.WriteLine("\nInformações da Agência:");
+            Console.WriteLine($"Número da Agência: {conta1.GetAgencia().Numero}");
+            Console.WriteLine($"CEP da Agência: {conta1.GetAgencia().CEP}");
+            Console.WriteLine($"Telefone da Agência: {conta1.GetAgencia().Telefone}");
+
+            
+            Console.WriteLine("\nInformações do Banco:");
+            Console.WriteLine($"Nome do Banco: {conta1.GetAgencia().Banco.Nome}");
+            Console.WriteLine($"Número do Banco: {conta1.GetAgencia().Banco.Numero}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+}
